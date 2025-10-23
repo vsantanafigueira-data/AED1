820575d9425438b0c3e57ff3ebde34f7c5725fa3
victor <vsantanafigueira@gmail.com>
2025-10-23 16:52:46 -0300

    criação do nó da lista encadeada simples ( sem cabeça)

---
+++ 4_6_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//lista encadeada sem cabeça é uma lista ligada onde o ponteiro inicial aponta diretamente para o primeiro nó da lista.
+//buscaeremove : função que irá buscar um elemento X na lista e removê-lo se encontrado.
+//sem cabeça : não existe um nó adicional no início da lista para facilitar operações.
+
+typedef struct no {
+    int valor;
+    struct no* prox;
+} No;
+
+
+int main(){
+
+
+
+    return 0;
+}
